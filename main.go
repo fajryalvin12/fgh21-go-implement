df09c3e810734031ed15a0ebbef295dc641994d9
fajryalvin12 <fajryalvin12@gmail.com>
2024-07-30 18:08:40 +0700

    feat: create matrix in main.go

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
+import "fmt"
+
 func main() {
 	// mainprocess.FazzFood(75000, "Ditraktir60", 5, true)
 	// mainprocess.Survey()
 	// mainprocess.Struct()
-
+	matrix := [][][]int{
+		{
+			{1,2,3,4},
+			{1,2,3,4},},
+		{},
+		{},
+		{
+			{1,2,10,4},
+			{1,2,3,4},
+		},
+	}
+	fmt.Println(matrix[3][0][2])
+	fmt.Println(matrix[0][1][2])
 }
